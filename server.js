e56a51f3f589713fdfcfd8307e7d275847b370c9
Annalyn Sarmiento <annalyn.c.s@gmail.com>
2017-12-09 16:07:18 -0800

    route to get all finance data

--- server.js
+++ server.js
@@ -7,13 +7,30 @@ const jsonParser = bodyParser.json();
 
 mongoose.Promise = global.Promise;
 
+const app = express();
+
 
 const {
     DATABASE_URL,
     PORT
 } = require('./config');
 
-const app = express();
+const {
+    Finance
+} = require('./models/finance-model');
+
+const {
+    Fitness
+} = require('./models/fitness-model');
+
+const {
+    Health
+} = require('./models/health-model');
+
+const {
+    Transport
+} = require('./models/transport-model');
+
 
 app.use(express.static('public'));
 app.use(morgan('common'));
@@ -34,12 +51,61 @@ app.use(function (req, res, next) {
     next();
 });
 
-app.get("/", (req,res) => {
-    console.log('it works!');
-    next();
-})
-
-app.listen(PORT, () => {
-    console.log(`Your app is listening on port ${PORT}`);
+app.get('/finances', (req, res, next) => {
+    Finance
+        .find()
+        .then(finances => {
+            res.json(finances);
+        })
+        .catch(err => {
+            console.error(err);
+            res.status(500).json({
+                error: 'someting went wrong'
+            });
+        });
 });
+
+let server;
+
+function runServer(databaseUrl = DATABASE_URL, port = PORT) {
+    return new Promise((resolve, reject) => {
+        mongoose.connect(databaseUrl, err => {
+            if (err) {
+                return reject(err);
+            }
+            server = app.listen(port, () => {
+                console.log(`Your app is listening on port ${port}`);
+                resolve();
+            })
+                .on('error', err => {
+                mongoose.disconnect();
+                reject(err);
+            });
+        });
+    });
+}
+
+function closeServer() {
+    return mongoose.disconnect().then(() => {
+        return new Promise((resolve, reject) => {
+            console.log('Closing server');
+            server.close(err => {
+                if (err) {
+                    return reject(err);
+                }
+                resolve();
+            });
+        });
+    });
+}
+
+if (require.main === module) {
+    runServer().catch(err => console.error(err));
+};
+
+module.exports = {
+    runServer,
+    app,
+    closeServer
+};
 
