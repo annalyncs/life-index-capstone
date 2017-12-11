2f7c32b517e1607496c84330ed8545b1c053f224
Annalyn Sarmiento <annalyn.c.s@gmail.com>
2017-12-10 17:02:43 -0800

    Click on edit button to edit entry

--- public/client.js
+++ public/client.js
@@ -13,16 +13,17 @@
             url: FINANCES_URL
         })
             .done(function(data) {
+                console.log(data);
                 let financeTableRows =
                     data.map(function (finance, index) {
-                        return `<tr class="data-rows">
+                        return `<tr class="data-rows" id=${finance._id}>
                             <td id="finance-date-row">${finance.date}</td>
                             <td id="finance-category-row">${finance.category}</td>
                             <td id="finance-cost-row">${finance.cost}</td>
                             <td id="finance-notes-row">${finance.notes}</td>
                             <td><a href="#"><img src="icons/view.png" alt="view-icon" class="icon view-icon" id="${finance._id}"></a></td>
                             <td><a href="#"><img src="icons/edit.png" alt="edit-icon" class="icon edit-icon" id="${finance._id}"></a></td>
-                            <td><a href="#"><img src="icons/delete.png" alt="delete-icon" class="icon edit-icon" id="{finance._id}"></a></td>
+                            <td><a href="#"><img src="icons/delete.png" alt="delete-icon" class="icon edit-icon" id="${finance._id}"></a></td>
                         </tr>`;
                     });
                 $('#finance-table').removeClass('hide-display');
@@ -38,7 +39,7 @@
             .done(function(data) {
             let healthTableRows =
                 data.map(function (health, index) {
-                    return `<tr class="data-rows">
+                    return `<tr class="data-rows" id=${health._id}>
                         <td id="health-date-row">${health.date}</td>
                         <td id="health-category-row">${health.category}</td>
                         <td id="health-notes-row">${health.notes}</td>
@@ -60,7 +61,7 @@
             .done(function(data) {
             let fitnessTableRows =
                 data.map(function (fitness, index) {
-                    return `<tr class="data-rows">
+                    return `<tr class="data-rows" id=${fitness._id}>
                         <td id="fitness-date-row">${fitness.date}</td>
                         <td id="fitness-workout-row">${fitness.workout}</td>
                         <td id="fitness-duration-row">${fitness.duration}</td>
@@ -83,7 +84,7 @@
             .done(function(data) {
             let transportTableRows =
                 data.map(function (transport, index) {
-                    return `<tr class="data-rows">
+                    return `<tr class="data-rows" id=${transport._id}>
                         <td id="fitness-date-row">${transport.date}</td>
                         <td id="fitness-type-row">${transport.type}</td>
                         <td id="fitness-miles-row">${transport.miles}</td>
@@ -366,9 +367,10 @@
     //first get data
     //then update
 
-    function retrieveFitnessData() {
+    function retrieveFinanceData() {
         $('#finance-table').on('click', '.edit-icon', function() {
             let idParameter =$(this).attr('id');
+            console.log(idParameter);
             $('#finance-table').addClass('hide-display');
             $('.data-controls').addClass('hide-display');
 
@@ -379,14 +381,14 @@
                 .done(function(data) {
                     console.log(data);
                     let financeUpdateForm =
-                        `<div id="update-entry-finance" class="add-entry hide-display">
+                        `<div id="update-entry-finance" class="add-entry">
                             <form class="add-entry" id=${idParameter}>
                                 <h1 class="add-entry-data">Finance</h1>
-                                <h2 class="add-entry-new">New Entry</h2>
+                                <h2 class="add-entry-new">Update Entry</h2>
                                 <label for="date">Date</label><br>
-                                <input type="text" name="date" value=${data.date}><br>
+                                <input type="text" name="date" value=${data.date} id="finance-date"><br>
                                 <label for="category">Category</label><br>
-                                <select name="category" value=${data.category}>
+                                <select name="category" value=${data.category} id="finance-category">
                                     <option>Apparel, Shoes & Accessories</option>
                                     <option>Dining & Drinking</option>
                                     <option>Entertainment</option>
@@ -399,19 +401,51 @@
                                     <option>Other</option>
                                 </select><br>
                                 <label for="cost">Cost</label><br>
-                                <input type="text" name="cost" value=${data.cost}><br>
+                                <input type="text" name="cost" value=${data.cost} id="finance-cost"><br>
                                 <label for="notes">Notes</label><br>
-                                <input type="text" name="notes" value=${data.notes}><br>
+                                <input type="text" name="notes" value=${data.notes} id="finance-notes"><br>
                                 <input type="submit" value="Update" class="add-submit finance-update">
                             </form>
                     </div>`;
-
-                $('#finance-dash-buttons').before(financeUpdateForm);
+                $('#finance-main').html(financeUpdateForm);
                 $('#update-entry-finance').removeClass('hide-display');
             });
         });
     }
 
-    retrieveFitnessData();
+    function updateFinanceData() {
+        $('#finance-data').on('click', '.finance-update', function(e) {
+            e.preventDefault();
+            let idParameter = $('form.add-entry').attr('id');
+            let dateInput = $('form.add-entry').find('#finance-date').val();
+            let categoryInput = $('form.add-entry').find('select#finance-category').val();
+            let costInput = $('form.add-entry').find('#finance-cost').val();
+            let notesInput = $('form.add-entry').find('#finance-notes').val();
+            let newDataInput = {
+                'date': dateInput,
+                'category': categoryInput,
+                'cost': costInput,
+                'notes': notesInput
+            }
+            console.log(idParameter);
+
+            $.ajax({
+                method: 'PUT',
+                url: `${FINANCES_URL}/${idParameter}`,
+                contentType: 'application/json',
+                data: JSON.stringify(newDataInput)
+            })
+                .done(function(data) {
+                    $('.data-rows').html('');
+                    displayAllFinanceEntries();
+                    $('#update-entry-finance').addClass('hide-display');
+                    $('#finance-table').removeClass('hide-display');
+                    $('.data-controls').removeClass('hide-display');
+            })
+        })
+    }
+
+    retrieveFinanceData();
+    updateFinanceData();
 
 })(jQuery);
