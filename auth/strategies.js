b48b8d4caaf7dade13109f035b522767198ef379
Annalyn Sarmiento <annalyn.c.s@gmail.com>
2017-12-13 22:19:14 -0800

    updated passport strategies

--- auth/strategies.js
+++ auth/strategies.js
@@ -3,7 +3,8 @@ const { Strategy: LocalStrategy } = require('passport-local');
 
 // Assigns the Strategy export to the name JwtStrategy using object destructuring
 // https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/Operators/Destructuring_assignment#Assigning_to_new_variable_names
-const { Strategy: JwtStrategy, ExtractJwt } = require('passport-jwt');
+const JwtStrategy = require('passport-jwt').Strategy,
+      ExtractJwt = require('passport-jwt').ExtractJwt;
 
 const { User } = require('../models/User');
 const { JWT_SECRET } = require('../config');
